e5c3d3b77b47125762202cce3043acd2a3ab8573
Pablo Alaniz <pablobvch@gmail.com>
2022-08-03 15:34:59 -0300

    first commit

---
+++ useCounter.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+export const useCounter = (initialValue = 10) => {
+  const [counter, setCounter] = useState(initialValue);
+
+  const increment = (value = 1) => {
+    setCounter((currentCounter) => currentCounter + value);
+  };
+  const decrement = (value = 1) => {
+    setCounter((currentCounter) => currentCounter - value);
+  };
+  const reset = () => {
+    setCounter(0);
+  };
+
+  return {
+    counter,
+    increment,
+    decrement,
+    reset
+  };
+};
